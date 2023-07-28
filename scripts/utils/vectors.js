380cbc107dfe5bbc2215461a93fe8ad689b26fdf
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2023-07-28 10:49:04 -0300

    Fixed accessing vectors values

--- scripts/utils/vectors.js
+++ scripts/utils/vectors.js
@@ -45,45 +45,45 @@ class Vec4{
 export {Vec2, Vec3, Vec4}
 
 function AddVec2(v1, v2){
-    return new Vec2(v1[0] + v2[0], v1[1] + v2[1]);
+    return new Vec2(v1.vec[0] + v2.vec[0], v1.vec[1] + v2.vec[1]);
 }
 function AddVec3(v1, v2){
-    return new Vec3(v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]);
+    return new Vec3(v1.vec[0] + v2.vec[0], v1.vec[1] + v2.vec[1], v1.vec[2] + v2.vec[2]);
 }
 function AddVec4(v1, v2){
-    return new Vec4(v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2], v1[3] + v2[3]);
+    return new Vec4(v1.vec[0] + v2.vec[0], v1.vec[1] + v2.vec[1], v1.vec[2] + v2.vec[2], v1.vec[3] + v2.vec[3]);
 }
 export {AddVec2, AddVec3, AddVec4}
 
 function SubVec2(v1, v2){
-    return new Vec2(v1[0] - v2[0], v1[1] - v2[1]);
+    return new Vec2(v1.vec[0] - v2.vec[0], v1.vec[1] - v2.vec[1]);
 }
 function SubVec3(v1, v2){
-    return new Vec3(v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]);
+    return new Vec3(v1.vec[0] - v2.vec[0], v1.vec[1] - v2.vec[1], v1.vec[2] - v2.vec[2]);
 }
 function SubVec4(v1, v2){
-    return new Vec4(v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2], v1[3] - v2[3]);
+    return new Vec4(v1.vec[0] - v2.vec[0], v1.vec[1] - v2.vec[1], v1.vec[2] - v2.vec[2], v1.vec[3] - v2.vec[3]);
 }
 export {SubVec2, SubVec3, SubVec4}
 
 function MultVec2(v1, v2){
-    return new Vec2(v1[0] * v2[0], v1[1] * v2[1]);
+    return new Vec2(v1.vec[0] * v2.vec[0], v1.vec[1] * v2.vec[1]);
 }
 function MultVec3(v1, v2){
-    return new Vec3(v1[0] * v2[0], v1[1] * v2[1], v1[2] * v2[2]);
+    return new Vec3(v1.vec[0] * v2.vec[0], v1.vec[1] * v2.vec[1], v1.vec[2] * v2.vec[2]);
 }
 function MultVec4(v1, v2){
-    return new Vec4(v1[0] * v2[0], v1[1] * v2[1], v1[2] * v2[2], v1[3] * v2[3]);
+    return new Vec4(v1.vec[0] * v2.vec[0], v1.vec[1] * v2.vec[1], v1.vec[2] * v2.vec[2], v1.vec[3] * v2.vec[3]);
 }
 export {MultVec2, MultVec3, MultVec4}
 
 function DivVec2(v1, v2){
-    return new Vec2(v1[0] / v2[0], v1[1] / v2[1]);
+    return new Vec2(v1.vec[0] / v2.vec[0], v1.vec[1] / v2.vec[1]);
 }
 function DivVec3(v1, v2){
-    return new Vec3(v1[0] / v2[0], v1[1] / v2[1], v1[2] / v2[2]);
+    return new Vec3(v1.vec[0] / v2.vec[0], v1.vec[1] / v2.vec[1], v1.vec[2] / v2.vec[2]);
 }
 function DivVec4(v1, v2){
-    return new Vec4(v1[0] / v2[0], v1[1] / v2[1], v1[2] / v2[2], v1[3] / v2[3]);
+    return new Vec4(v1.vec[0] / v2.vec[0], v1.vec[1] / v2.vec[1], v1.vec[2] / v2.vec[2], v1.vec[3] / v2.vec[3]);
 }
 export {DivVec2, DivVec3, DivVec4}
